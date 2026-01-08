8810d9c2f5dd383d6cd56f8a8537d9da6e21d6d3
PuszkaPotato <kolodziej.weronika@proton.me>
2026-01-08 03:37:48 +0100

    refactor: reduce config boilerplate with SetandRefresh helper

--- Bannerlord.ShipmasterReworked/Settings/MCMConfig.cs
+++ Bannerlord.ShipmasterReworked/Settings/MCMConfig.cs
@@ -8,168 +8,87 @@ namespace Bannerlord.ShipmasterReworked.Settings
     {
         private float _travelXpMultiplier = 3.0f;
         private bool _travelXpDebug = false;
-
         private bool _enableStormTravelXp = true;
         private float _stormTravelXpMultiplier = 2f;
-
-        private int _rammingXpBase = 80;    
+        private int _rammingXpBase = 80;
         private float _rammingXpQualityFactor = 0.15f;
         private bool _rammingXpDebug = false;
-        
+
         public override string Id => ModuleInfo.Id;
         public override string DisplayName => ModuleInfo.DisplayName + " " + ModuleInfo.Version;
         public override string FolderName => ModuleInfo.FolderName;
         public override string FormatType => ModuleInfo.FormatType;
 
-        [SettingPropertyFloatingInteger(
-            "Travel XP Multiplier",
-            0.1f,
-            10f,
-            "#0.00",
-            Order = 1,
-            RequireRestart = false,
+        [SettingPropertyFloatingInteger("Travel XP Multiplier", 0.1f, 10f, "#0.00", Order = 1, RequireRestart = false,
             HintText = "Multiplier for Shipmaster XP gained from traveling.")]
         [SettingPropertyGroup("Travel Multiplier Settings")]
         public float TravelXpMultiplier
         {
             get => _travelXpMultiplier;
-            set 
-            {
-                if (_travelXpMultiplier != value)
-                {
-                    _travelXpMultiplier = value;
-                    ConfigCache.Refresh(this);
-                    OnPropertyChanged();
-                }
-            }
+            set => SetAndRefresh(ref _travelXpMultiplier, value);
         }
 
-        // Do we apply bonus from travelling in storm?
-        [SettingPropertyBool(
-            "Enable Storm Travel Bonus",
-            Order = 2,
-            RequireRestart = false,
+        [SettingPropertyBool("Enable Storm Travel Bonus", Order = 2, RequireRestart = false,
             HintText = "If enabled, traveling in stormy weather will provide additional Shipmaster XP.")]
         [SettingPropertyGroup("Travel Multiplier Settings")]
-        public bool EnableStormTravelXp 
+        public bool EnableStormTravelXp
         {
             get => _enableStormTravelXp;
-            set 
-            {
-                if (_enableStormTravelXp != value)
-                {
-                    _enableStormTravelXp = value;
-                    ConfigCache.Refresh(this);
-                    OnPropertyChanged();
-                }
-            }
+            set => SetAndRefresh(ref _enableStormTravelXp, value);
         }
 
-        [SettingPropertyFloatingInteger(
-            "Storm Travel XP Bonus Multiplier",
-            0.1f,
-            10.0f,
-            "#0.00",
-            Order = 3,
-            RequireRestart = false,
+        [SettingPropertyFloatingInteger("Storm Travel XP Bonus Multiplier", 0.1f, 10.0f, "#0.00", Order = 3, RequireRestart = false,
             HintText = "Additional multiplier for Shipmaster XP when traveling in stormy weather.")]
         [SettingPropertyGroup("Travel Multiplier Settings")]
         public float StormTravelXpMultiplier
         {
             get => _stormTravelXpMultiplier;
-            set 
-            {
-                if (_stormTravelXpMultiplier != value)
-                {
-                    _stormTravelXpMultiplier = value;
-                    ConfigCache.Refresh(this);
-                    OnPropertyChanged();
-                }
-            }
+            set => SetAndRefresh(ref _stormTravelXpMultiplier, value);
         }
 
-        [SettingPropertyBool(
-            "Enable Travel XP Debug Messages",
-            Order = 9,
-            RequireRestart = false,
+        [SettingPropertyBool("Enable Travel XP Debug Messages", Order = 9, RequireRestart = false,
             HintText = "If enabled, debug messages will be displayed when gaining Shipmaster XP from traveling.")]
         [SettingPropertyGroup("Travel Multiplier Settings")]
         public bool TravelXpDebug
         {
             get => _travelXpDebug;
-            set 
-            {
-                if (_travelXpDebug != value)
-                {
-                    _travelXpDebug = value;
-                    ConfigCache.Refresh(this);
-                    OnPropertyChanged();
-                }
-            }
+            set => SetAndRefresh(ref _travelXpDebug, value);
         }
 
-        [SettingPropertyInteger(
-            "Ramming XP Base",
-            0,
-            100,
-            Order = 1,
-            RequireRestart = false,
+        [SettingPropertyInteger("Ramming XP Base", 0, 100, Order = 1, RequireRestart = false,
             HintText = "Base XP gained from ramming before multipliers.")]
         [SettingPropertyGroup("Ramming XP Settings")]
         public int RammingXpBase
         {
             get => _rammingXpBase;
-            set 
-            {
-                if (_rammingXpBase != value)
-                {
-                    _rammingXpBase = value;
-                    ConfigCache.Refresh(this);
-                    OnPropertyChanged();
-                }
-            }
+            set => SetAndRefresh(ref _rammingXpBase, value);
         }
 
-        [SettingPropertyFloatingInteger(
-            "Ramming XP Quality Factor",
-            0.0f,
-            1.0f,
-            "#0.00",
-            Order = 2,
-            RequireRestart = false,
+        [SettingPropertyFloatingInteger("Ramming XP Quality Factor", 0.0f, 1.0f, "#0.00", Order = 2, RequireRestart = false,
             HintText = "Multiplier factor for ramming quality affecting XP gained.")]
         [SettingPropertyGroup("Ramming XP Settings")]
         public float RammingXpQualityFactor
         {
             get => _rammingXpQualityFactor;
-            set 
-            {
-                if (_rammingXpQualityFactor != value)
-                {
-                    _rammingXpQualityFactor = value;
-                    ConfigCache.Refresh(this);
-                    OnPropertyChanged();
-                }
-            }
+            set => SetAndRefresh(ref _rammingXpQualityFactor, value);
         }
 
-        [SettingPropertyBool(
-            "Enable Ramming XP Debug Messages",
-            Order = 9,
-            RequireRestart = false,
+        [SettingPropertyBool("Enable Ramming XP Debug Messages", Order = 9, RequireRestart = false,
             HintText = "If enabled, debug messages will be displayed when gaining Shipmaster XP from ramming.")]
         [SettingPropertyGroup("Ramming XP Settings")]
         public bool RammingXpDebug
         {
             get => _rammingXpDebug;
-            set 
+            set => SetAndRefresh(ref _rammingXpDebug, value);
+        }
+
+        private void SetAndRefresh<T>(ref T field, T value)
+        {
+            if (!Equals(field, value))
             {
-                if (_rammingXpDebug != value)
-                {
-                    _rammingXpDebug = value;
-                    ConfigCache.Refresh(this);
-                    OnPropertyChanged();
-                }
+                field = value;
+                ConfigCache.Refresh(this);
+                OnPropertyChanged();
             }
         }
     }
